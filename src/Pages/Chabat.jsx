92fffc12a1a234c312b362335038ad2c6185b729
David <bensadondavidn@gmai.com>
2025-03-04 15:41:11 +0100

    changes

--- src/Pages/Chabat.jsx
+++ src/Pages/Chabat.jsx
@@ -9,22 +9,18 @@ function Chabat(){
 
     return(
 
-        <div className="chabat">
-           <div className="chabat-card">
-                <img src="fleur-fonce-haut.png" className='fleur-fonce-haut' />
-                <h1 className='chabat-title'>LE CHABAT</h1>
-                <img src="logo-chabat.png" className='logo-chabat'/>
+        <div className="arche">
+           <div className="arche-card">
+                <img src="arche-m6.png" className='arche-image' />
+                <h1 className='chabat-title'>Le Chabat</h1>
                 <div className="chabat-noms">
-                    <h1 className='chabat-nom-1'>Victoria</h1>
-                    <h1 className='chabat-and'>&</h1>
-                    <h1 className='chabat-nom-2'>Alone</h1>
+                    <p>Noa & David</p>
                 </div>
                 <p className='chabat-text'>Seront ravis de partager leur chabat hatan avec vous </p>
                 <p className='chabat-date'>Le vendredi 8 <br /> et samedi 9 juin 2024</p>
-                <p className='chabat-endroit'>A la synagogue Shaare Elyahou 13 guideon haozner<br />Jérusalem</p>
-                <Waze name={'waze-chabat'} lat={48.8525} long={2.3372} iconWidth='18px' iconHeight='25px' iconColor='#C6A58A' />
-                <SaveTheDate name={'save-the-date-chabat'} date={[2024, 6, 3, 12, 0]} iconColor='#C6A58A' iconHeight='18px' iconWidth='21px'/>
-                <img src="fleur-fonce-bas.png" className='fleur-fonce-bas' />
+                <p className='chabat-endroit'>A la synagogue Shaare Elyahou <br /> 13 guideon haozner<br />Jérusalem</p>
+                <Waze name={'waze-chabat'} lat={48.8525} long={2.3372} iconWidth='18px' iconHeight='25px' iconColor='#536493' />
+                <SaveTheDate name={'save-the-date-chabat'} date={[2024, 6, 3, 12, 0]} iconColor='#536493' iconHeight='18px' iconWidth='21px'/>
             </div>
         </div>
 
